126a6440d04ada35cb73ad6fd9e24760791680f0
ttd9680660 <99306253+ttd9680660@users.noreply.github.com>
2022-05-31 10:35:45 +0700

    login

---
+++ ASS/src/java/controller/LoginController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dal.LecturersDBContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.Lecturers;
+
+/**
+ *
+ * @author Trung Duc
+ */
+public class LoginController extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        LecturersDBContext db = new LecturersDBContext();
+        Lecturers lec = db.getT(username, password);
+
+        if (lec == null) {
+            response.getWriter().println("Login Faild");
+        } else {
+            response.getWriter().println("Login Success");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+    
+   
+}
---
+++ ASS/src/java/dal/LecturersDBContext.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Lecturers;
+
+/**
+ *
+ * @author Trung Duc
+ */
+public class LecturersDBContext extends DBContext<Lecturers>{
+
+    @Override
+    public ArrayList<Lecturers> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Lecturers get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insert(Lecturers model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Lecturers model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Lecturers model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Lecturers getT(String username, String password) {
+try {
+            String sql = "select * from Login_Lecturers where username = ? and password = ?";          
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, username);
+            stm.setString(2, password);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next())
+            {
+                Lecturers lec = new Lecturers();
+                lec.setUsername(rs.getString("username"));
+                lec.setPassword(rs.getString("password"));               
+                lec.setDisplayname(rs.getString("displayname"));
+                return lec;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(LecturersDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;    }
+    
+}
